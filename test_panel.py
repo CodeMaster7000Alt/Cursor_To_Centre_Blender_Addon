c51ffd22456f4971a5515df73abea53d1eeea4ad
CodeMaster7000Alt <99122448+CodeMaster7000Alt@users.noreply.github.com>
2022-12-19 10:57:15 +0000

    Create test_panel.py

---
+++ test_panel.py
@@ -0,0 +1,11 @@
+import bpy
+class Test_PT_Panel(bpy.types.Panel):
+    bl_idname = "Test_PT_Panel"
+    bl_label = "Test Panel"
+    bl_category = "Test Add-on"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.operator("view3d.cursor_center", text="Send cursor to center")
